17db5d4cabc26f4ecedf914e76e1160fbeaa6dbd
Jose Luis Gimeno <jgimenoperez@gmail.com>
2024-07-21 21:21:38 +0200

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded",(evet)=>{
     
     let input_search = document.querySelector(".search__input")
     let btn_search = document.querySelector(".navbar__btn-search")
-
+    let mobile_btn = document.querySelector(".navbar__mobile-btn")
+    let mobile_menu = document.querySelector(".navbar__mobile-list")
+    let drop_down1_btn = document.querySelector("#icon1")
+    let drop_down1_submenu = document.querySelector("#submenu1")
+    let drop_down2_btn = document.querySelector("#icon2")
+    let drop_down2_submenu = document.querySelector("#submenu2")
 
     let marquee = (selector, speed)=>{
         const container = document.querySelector(selector);
@@ -18,10 +23,26 @@ document.addEventListener("DOMContentLoaded",(evet)=>{
 
         }, 0);
     }
+
+    const submenu = (boton ,submenu, className)=>{
+        boton.addEventListener("click", () =>{
+            let drop= document.querySelector("."+className);
+
+            if(!drop){
+                submenu.style.display="block";
+                mobile_menu.classList.add(className)
+
+            }
+            else{
+                submenu.style.display="none";
+                mobile_menu.classList.remove(className)
+            }
+        })
+    }
+
     marquee(".marquee_container",0.2);
 
     btn_search.addEventListener("click",()=>{
-        console.log(1)
         input_search.classList.contains("search__input--visible")
         ? (() => {
 
@@ -47,6 +68,31 @@ document.addEventListener("DOMContentLoaded",(evet)=>{
         // input_search.classList.remove("visible")
     })
 
+    mobile_btn.addEventListener("click",()=>{
+       let menu_open= document.querySelector(".menu_open")
+
+       if(!menu_open) {
+        mobile_menu.style.display="block";
+        mobile_menu.classList.add("menu_open")
+       }
+       else{
+        mobile_menu.style.display="none";
+        mobile_menu.classList.remove("menu_open")
+       }
+    })
+
+    submenu(drop_down1_btn,drop_down1_submenu,"drop1")
+    submenu(drop_down2_btn,drop_down2_submenu,"drop2")
+
+    window.addEventListener(resize , () =>{
+        let win = parseFloat(document.body.clientWidth)
+
+        if (win > 1020){
+            mobile_menu.style.display="none";
+            mobile_menu.classList.remove("menu_open")
+        }
+            
+    })
 
 
 })
